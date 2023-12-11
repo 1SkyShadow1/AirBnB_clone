cf81159544d4aa5a29fcaba8ba6b8b754a97ca4a
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-12-11 06:16:11 -0800

    Update for baseModel

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
 
     def __str__(self) -> str:
         """Returns the string representation of an instance"""
-        return "[{}] [{}] [{}]".format(
+        return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
