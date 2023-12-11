f5d0aa5d3191e514823734b20d335c18e916fbbf
JonaNdawula <jonathan.ndawula@yahoo.com>
2023-12-11 06:09:03 -0800

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,8 @@ class BaseModel:
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key != "__class__":
                     self.__dict__[key] = value
-            else:
-                models.storage.new(self)
+        else:
+            models.storage.new(self)
 
     def __str__(self) -> str:
         """Returns the string representation of an instance"""
@@ -48,6 +48,6 @@ class BaseModel:
     to_dict["__class__"] = self.__class__.__name__
     if not isinstance(to_dict["created_at"], str):
         to_dict["created_at"] = to_dict["created_at"].isoformat()
-        if not isinstance(to_dict["updated_at"], str):
-            to_dict["updated_at"] = to_dict["updated_at"].isoformat()
-            return to_dict
+    if not isinstance(to_dict["updated_at"], str):
+        to_dict["updated_at"] = to_dict["updated_at"].isoformat()
+    return to_dict
